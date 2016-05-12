97c42845ea5f42229076958e5b64309b46b98a3a
Sand2007 <sand2007@i.ua>
2016-05-12 11:13:00 +0300

    ITerico on Bootstrap version

---
+++ js/scrollTracking.js
@@ -0,0 +1,20 @@
+﻿//При прокручивании странички вниз меняется внешний вид меню
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 50) {
+        $('header').addClass('headerScroll');
+        $('.header-content').addClass('headerContentScroll');        
+    } else {
+        $('header').removeClass('headerScroll');
+        $('.header-content').removeClass('headerContentScroll');        
+    }
+});
+
+//При наведении на технологию показывается ее название при помощи слайда вниз
+$('.boxShowHide').hover(
+    function () {
+        $(this).find('.caption').slideDown(450);
+    },
+    function () {
+        $(this).find('.caption').slideUp(200);
+    }
+    );
